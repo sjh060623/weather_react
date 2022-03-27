d74ddb09b31a80721cc20042c4efee39c273803d
손정훈 <sjh060623@gmail.com>
2022-03-27 22:12:39 +0900

    weather 1

--- pre2.js
+++ pre2.js
@@ -1,22 +1,3 @@
-import * as Location from "expo-location";
-import React, { useEffect, useState } from "react";
-import { View, Text, Dimensions, StyleSheet, ScrollView } from "react-native";
-import { Fontisto } from "@expo/vector-icons";
-
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
-
-const API_KEY = "ac1fe6259a80a43a841f7ca9d2e30521";
-
-const icons = {
-  Clouds: "cloudy",
-  Clear: "day-sunny",
-  Atmosphere: "cloudy-gusts",
-  Snow: "snow",
-  Rain: "rains",
-  Drizzle: "rain",
-  Thunderstorm: "lightning",
-};
-
 export default function App() {
   const [city, setCity] = useState("Loading");
   const [days, setDays] = useState([]);
@@ -40,20 +21,20 @@ export default function App() {
   useEffect(() => {
     getWeather();
   }, []);
-  
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
         <Text style={styles.cityName}>{city}</Text>
       </View>
-      <ScrollView pagingEnabled horizontal>
+      <ScrollView horizontal pagingEnabled>
         {days.map((day, i) => (
           <View key={i} style={styles.day}>
             <Text style={styles.temp}>{day.temp.day}</Text>
             <Fontisto
+              name={icons[day.weather[0].main]}
               size={68}
               color="white"
-              name={icons[day.weather[0].main]}
             />
             <Text style={styles.description}>{day.weather[0].main}</Text>
           </View>
@@ -62,36 +43,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#161e38",
-  },
-  city: {
-    flex: 1.2,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  cityName: {
-    fontSize: 58,
-    fontWeight: "500",
-    color: "white",
-  },
-  day: {
-    width: SCREEN_WIDTH,
-    alignItems: "flex-start",
-    paddingHorizontal: 20,
-  },
-  temp: {
-    fontWeight: "600",
-    fontSize: 80,
-    color: "white",
-  },
-  description: {
-    marginTop: -10,
-    fontSize: 30,
-    color: "white",
-    fontWeight: "500",
-  },
-});
